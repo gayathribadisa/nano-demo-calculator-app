f48dc4cbf7daf26e65ec3aa76507c92f7e7ae13c
gayathribadisa <144468937+gayathribadisa@users.noreply.github.com>
2023-09-09 11:35:33 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -1,29 +1,57 @@
 #include "crow_all.h"
-crow::response greet(){
-    crow::response res;    res.code = 200;
-    res.set_header("Content-Type", "text/plain");    res.body = "Hello world!";
-        return res;
-}crow::response add(const crow::request &req)
-{    auto input = crow::json::load(req.body);
-    if (!input ||  !input.has("first") || !input.has("second")) {        return crow::response(400, "Invalid JSON format");
-    }    int first = input["first"].i();
-    int second = input["second"].i();    
+
+crow::response greet()
+{
+    crow::response res;
+    res.code = 200;
+    res.set_header("Content-Type", "text/plain");
+    res.body = "Hello world!";
+    
+    return res;
+}
+crow::response add(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+    if (!input || !input.has("first") || !input.has("second")) {
+        return crow::response(400, "Invalid JSON format");
+    }
+    int first = input["first"].i();
+    int second = input["second"].i();
+    
+
     int result = first + second;
+
     std::ostringstream oss;
     oss << "{\"result\":" << result << "}";
-    // Return the response with status code 200 and JSON content    crow::response res;
-    res.code = 200;    res.set_header("Content-Type", "application/json");
-    res.body = oss.str();
-    return res;}
-crow::response subtract(const crow::request &req){
-    auto input = crow::json::load(req.body);     if (!input || !input.has("first") || !input.has("second")) {
-        return crow::response(400, "Invalid JSON format");    }
-    int first = input["first"].i();    int second = input["second"].i();
-    
-    int result = first - second;
-    std::ostringstream oss;    oss << "{\"result\":" << result << "}";
+
     // Return the response with status code 200 and JSON content
-    crow::response res;    res.code = 200;
-    res.set_header("Content-Type", "application/json");    res.body = oss.str();
+    crow::response res;
+    res.code = 200;
+    res.set_header("Content-Type", "application/json");
+    res.body = oss.str();
+
+    return res;
+}
+crow::response subtract(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+     if (!input || !input.has("first") || !input.has("second")) {
+        return crow::response(400, "Invalid JSON format");
+    }
+    int first = input["first"].i();
+    int second = input["second"].i();
+    
+
+    int result = first - second;
+
+    std::ostringstream oss;
+    oss << "{\"result\":" << result << "}";
+
+    // Return the response with status code 200 and JSON content
+    crow::response res;
+    res.code = 200;
+    res.set_header("Content-Type", "application/json");
+    res.body = oss.str();
+
     return res;
 }
