9db8282da2efa2f1d1f6450553e4bff5c259c3b2
gayathribadisa <144468937+gayathribadisa@users.noreply.github.com>
2023-09-12 11:35:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,33 @@
-from flask import Flask, request, jsonify 
- 
-app = Flask(__name__) 
- 
-@app.route("/calculator/greeting", methods=['GET']) 
-def greeting(): 
-    return "Hello, World!", 200 
- 
-@app.route("/calculator/add", methods=['POST']) 
-def add(): 
-    try: 
-        data = request.get_json() 
-        if "first" in data and "second" in data: 
-            first = data["first"] 
-            second = data["second"] 
-             result = first + second 
-            response_data = {"result": result} 
-            return jsonify(response_data), 200 
-        else: 
-            return "Invalid JSON format", 400 
-    except Exception as e: 
-        return str(e), 500 
- 
-@app.route("/calculator/subtract", methods=['POST']) 
-def subtract(): 
-    try: 
-        data = request.get_json() 
-        if "first" in data and "second" in data: 
-            first = data["first"] 
-            second = data["second"] 
-            result = first - second 
-            response_data = {"result": result} 
-            return jsonify(response_data), 200 
-        else: 
-            return "Invalid JSON format", 400 
-    except Exception as e:
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/calculator/greeting", methods=['GET'])
+def greeting():
+    return 'Hello world!'
+    
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+    
+
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
+    
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
