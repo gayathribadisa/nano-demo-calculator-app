69d7229bf56ac0cc17cd8297283ed4c23e9f5a63
gayathribadisa <2000031407cse@gmail.com>
2023-09-13 10:47:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,37 @@
 from flask import Flask, request, jsonify
-from dataclasses import dataclass
-
-@dataclass
-class Result:
-    result: int
-
-
 
 app = Flask(__name__)
 
-
-@app.route("/calculator/greeting", methods=['GET'])
+# Endpoint to check if the service is available
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!'
-    
+    return "Hello world!"
 
-@app.route("/calculator/add", methods=['POST'])
-def add():
-    numbers = request.json
-    response = Result(numbers['first'] + numbers['second'])
-    return jsonify(response)
-    
+# Endpoint to add two numbers
+@app.route('/calculator/add', methods=['POST'])
+def add_numbers():
+    try:
+        data = request.get_json()
+        first = data['first']
+        second = data['second']
+        result = first + second
+        response = {'result': result}
+        return jsonify(response)
+    except KeyError:
+        return "Invalid request. Please provide 'first' and 'second' numbers in the request body.", 400
 
-@app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    numbers = request.json
-    response = Result(numbers['first'] - numbers['second'])
-    return jsonify(response)
-    
+# Endpoint to subtract two numbers
+@app.route('/calculator/subtract', methods=['POST'])
+def subtract_numbers():
+    try:
+        data = request.json
+        first = data['first']
+        second = data['second']
+        result = first - second
+        response = {'result': result}
+        return jsonify(response)
+    except KeyError:
+        return "Invalid request. Please provide 'first' and 'second' numbers in the request body.", 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
